684fca2fa217c5296fcb602c245d905a6cd237a6
mennamohamedsalah <152805915+mennamohamedsalah@users.noreply.github.com>
2024-05-26 00:11:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,203 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// @media (min-width: 400px) {
+//   .badyindex{
+//       background-color: #ffffff;
+//       font-family: 'Poppins', sans-serif;
+//       height: 11225px;
+//       flex-wrap: wrap;
+//       overflow: hidden;
+//   }
+// }
+
+// @media (min-width: 500px) {
+//   .badyindex{
+//       background-color: #ffffff;
+//       font-family: 'Poppins', sans-serif;
+//       height: 11100px;
+//       flex-wrap: wrap;
+//       overflow: hidden;
+//   }
+// }
+
+
+// @media (min-width: 600px) {
+//   .badyindex{
+//       background-color: #ffffff;
+//       font-family: 'Poppins', sans-serif;
+//       height: 11040px;
+//       flex-wrap: wrap;
+//       overflow: hidden;
+//   }
+// }
+
+// @media (min-width: 700px) {
+//   .badyindex{
+//       background-color: #ffffff;
+//       font-family: 'Poppins', sans-serif;
+//       height: 11050px;
+//       flex-wrap: wrap;
+//       overflow: hidden;
+//   }
+// }
+
+
+// @media (min-width: 800px) {
+//   .badyindex{
+//       background-color: #ffffff;
+//       font-family: 'Poppins', sans-serif;
+//       /* height: 109000px; */
+//       flex-wrap: wrap;
+//       overflow: hidden;
+//   }
+// }
+
+
+
+// @media (min-width: 900px) {
+//   .badyindex{
+//       background-color: #ffffff;
+//       font-family: 'Poppins', sans-serif;
+//       height: 6000px;
+//       flex-wrap: wrap;
+//       overflow: hidden;
+//   }
+// }
+
+
+
+
+// @media only screen and (min-width:300px) and (max-width:700px){
+//   .laptop{
+//       display: none;
+//       }
+// }
+
+
+
+// @media only screen and (min-width:300px) and (max-width:700px){
+//   .h3p{
+// width: 230%;      
+// margin-left: -100%;  }
+// }
+
+
+// @media (max-width: 400px) {
+//   .b-Inizia{
+//       margin-top: 3%;
+//       margin-left: 4%;
+//       color: #ffffff;
+//       font-size: 14px;
+//       font-weight: 700;
+//       width: 20%;
+//       padding: 1%;
+//       border: none;
+//       border-radius: 4px;
+//       background-color: hsl(9, 84%, 63%);
+//   }
+  
+//     }
+
+
+
+
+//     @media (max-width: 400px) {
+//       .b-Leggi{
+//           margin-top: 3%;
+//           margin-left: 4%;
+//           color: #ffffff;
+//           font-size: 14px;
+//           font-weight: 700;
+//           width: 20%;
+//           padding: 1%;
+//           border: none;
+//           border-radius: 4px;
+//           background-color: hsl(9, 84%, 63%);
+//       }
+      
+//           }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
